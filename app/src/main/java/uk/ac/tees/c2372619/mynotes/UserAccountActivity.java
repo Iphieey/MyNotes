dad6d85c65a25353e89d874a20613eaa9f7b7eb2
Iphieey <iphiemoks@gmail.com>
2023-04-30 13:39:51 +0100

    MyNotes commit 1.1

--- app/src/main/java/uk/ac/tees/c2372619/mynotes/UserAccountActivity.java
+++ app/src/main/java/uk/ac/tees/c2372619/mynotes/UserAccountActivity.java
@@ -1,13 +1,21 @@
 package uk.ac.tees.c2372619.mynotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserAccountActivity extends AppCompatActivity {
 
@@ -47,11 +55,34 @@ public class UserAccountActivity extends AppCompatActivity {
     }
 
     void createAccountInFirebase(String email,String password){
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(UserAccountActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(UserAccountActivity.this, "Successfully created account,Check email to verify",Toast.LENGTH_SHORT).show();
+                    firebaseAuth.getCurrentUser().sendEmailVerification();
+                    firebaseAuth.signOut();
+                    finish();
+                }else{
+                    Toast.makeText(UserAccountActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
 
     }
 
     void changeInProgress(boolean inProgress){
-
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountButton.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountButton.setVisibility(View.VISIBLE);
+        }
     }
 
     boolean validateData(String email,String password, String confirmPassword){
